063be399d287137f67e80fce0814709d869aa12c
AntonVoinilovich <anton.voinilovich@gmail.com>
2021-03-16 02:12:23 +0300

    fix: add full task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix) return [];
+  let arrow = [];
+  let arrowNew = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2) arrow.push(matrix[i].reverse());
+    else arrow.push(matrix[i]);
+  }
+  for (let i = 0; i < arrow.length; i++) {
+    for (let ii = 0; ii < arrow[i].length; ii++) {
+      arrowNew.push(arrow[i][ii]);
+    }
+  }
+  return arrowNew;
 }
+
+
